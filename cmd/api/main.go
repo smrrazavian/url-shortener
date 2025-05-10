bee6cac73d8306c04b194b4cc81ed114b843cf2d
Mohammadreza Razavian <smrrazavian@outlook.com>
2025-05-10 09:24:24 +0330

    feat: add http server and router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/smrrazavian/url-shortener/pkg/idgen"
+	"github.com/smrrazavian/url-shortener/internal/config"
+	"github.com/smrrazavian/url-shortener/internal/router"
 )
 
 func main() {
-	id, _ := idgen.GenerateID()
-	fmt.Println(id)
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+	fmt.Println("Starting server...")
+
+	mux := http.NewServeMux()
+	router.AddRoutes(mux)
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.ServerPort),
+		Handler: mux,
+	}
+
+	fmt.Println("Listened on port", cfg.ServerPort)
+
+	server.ListenAndServe()
 }
---
+++ internal/router/routers.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"net/http"
+)
+
+func AddRoutes(
+	mux *http.ServeMux,
+) {
+	// mux.HandleFunc("/save", handlers.SaveURLHandler())
+}
